d7cf97605e1e370e0bf9b4ef9fc979fd8341325b
liugq <liugq5713@gmail.com>
2019-06-20 17:50:44 +0800

    feat: could gen  nest data

--- src/views/FormEditor/JsonForm.vue
+++ src/views/FormEditor/JsonForm.vue
@@ -16,6 +16,7 @@
 </template>
 
 <script>
+import { isObject, isArray } from "@/utils";
 export default {
   props: {
     store: Object
@@ -26,20 +27,47 @@ export default {
     };
   },
   methods: {
-    gen() {
-      try {
-        const formData = eval(`(${this.formJson})`);
-        const formItems = Object.keys(formData).map(item => {
-          return {
+    flattening(data, pre_value) {
+      if (!isObject(data)) {
+        this.$message.error("对象格式不正确");
+      }
+      if (isArray(data)) {
+        this.$message.error("a对象格式不正确");
+      }
+      let tmp = [];
+      Object.keys(data).map(item => {
+        if (isObject(data[item])) {
+          if (isArray(data[item])) {
+            data[item].map((arr_item, idx) => {
+              const pre = pre_value ? `${pre_value}${item}[${idx}].` : `${item}[${idx}].`;
+              const res = this.flattening(arr_item, pre);
+              tmp = tmp.concat(res);
+            });
+          } else {
+            let pre = pre_value ? `${pre_value}${item}.` : `${item}.`;
+            const res = this.flattening(data[item], pre);
+            tmp = tmp.concat(res);
+          }
+        } else {
+          const value = pre_value ? `${pre_value}${item}` : item;
+          tmp.push({
             type: "EInputTrim",
             title: "JSON生成",
             props: {
-              label: `${formData[item] || item}`,
-              value: item,
+              label: `${data[item] || item}`,
+              value,
               required: ""
             }
-          };
-        });
+          });
+        }
+      });
+      return tmp;
+    },
+
+    gen() {
+      try {
+        const formData = eval(`(${this.formJson})`);
+        const formItems = this.flattening(formData);
         this.store.commit("setFormItems", formItems);
       } catch (error) {
         this.$message.error("请填入正确的Object");
