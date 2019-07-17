03bf291f22e949960effb4d8ed8eb328d797fd3f
liugq <liugq5713@gmail.com>
2019-07-17 18:15:56 +0800

    feat[table]: add snipper

--- src/views/TableEditor/snippetTableColumn.js
+++ src/views/TableEditor/snippetTableColumn.js
@@ -1,5 +1,20 @@
 export const genTableColumnSnippet = headers => {
   headers.map(header => {
-    return header;
+    genTabeleColumSinppetTemplate(header.label)
+    return genTableColumSnippetSimple(header.value,header.label);
   });
 };
+
+const genTableColumSnippetSimple = (value,label)=>{
+  return `<el-table-column prop='${value}' label='${label}' align='center' />`
+}
+
+const genTabeleColumSinppetTemplate=(value,label,options)=>{
+  console.log('options', options)
+  return `<el-table-column label='${label}'  align='center'>
+  <template slot-scope={row}>
+    {{ ${value} }}
+  </template>
+</el-table-column>`
+}
+
