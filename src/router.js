8783f333235ddd18e396120f874cc736e6e30c52
liugq <liugq5713@gmail.com>
2019-07-18 13:18:45 +0800

    fix[router]: mode

--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ import Router from "vue-router";
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
+  mode: "hash",
   routes: [
     {
       path: "/",
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === 'production' ? '/vue-element-form-editor/' : '/'
-}
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-element-form-editor/" : "/"
+};
