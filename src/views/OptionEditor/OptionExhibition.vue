0daf3b74b7b6fe7594ac7a68c759406a66345fa8
liugq <liugq5713@gmail.com>
2019-07-22 18:33:23 +0800

    todo[options]

--- src/views/OptionEditor/OptionExhibition.vue
+++ src/views/OptionEditor/OptionExhibition.vue
@@ -1,7 +1,39 @@
 <template>
-  <div></div>
+  <div>
+    <pre>
+      <code class="xml">
+       
+      </code>
+    </pre>
+  </div>
 </template>
 
 <script>
-export default {};
+import {isObject,isArray} from '@/utils'
+import hljs from 'highlight.js/lib/highlight';
+import xml from 'highlight.js/lib/languages/xml';
+import 'highlight.js/styles/github.css';
+
+export default {
+  props:{
+    options:{
+      type:Object,
+    }
+  },
+  created(){
+    hljs.registerLanguage('xml', xml);
+     hljs.initHighlightingOnLoad();
+    console.log('hljs', hljs)
+  },
+  methods:{
+    genOptions(options){
+      if(isObject(options)){
+        return
+      }
+      if(isArray(options)){
+        return
+      }
+    }
+  }
+};
 </script>
