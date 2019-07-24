ffec93fd6ef23bee106c5e415333d103f5b138c3
liugq <liugq5713@gmail.com>
2019-07-24 18:00:36 +0800

    style[dashboard]

--- src/views/Dashboard/index.vue
+++ src/views/Dashboard/index.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <el-card class='box-card'>
+       
     <h1>关于这个项目</h1>
     <p>这个项目我给他起了一个很大很拉风的名字，nocode-admin ，希望能够把自己从一些重复劳动中解救出来</p>
     <h3>项目计划</h3>
@@ -26,6 +28,7 @@
         <a href="https://juejin.im/post/5cd50849f265da03a54c3877">为管理复杂组件状态困扰？试试 vue 简单状态管理 Store 模式</a>（项目初始的时候我这样使用的，目前已经移除了）
       </li>
     </ul>
+    </el-card>
   </div>
 </template>
 
