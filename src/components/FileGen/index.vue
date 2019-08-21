a3c893a968df0147251545c9e6266caaffa3c8ab
liugq <liugq5713@gmail.com>
2019-08-21 18:33:45 +0800

    feat[gen]

---
+++ src/components/FileGen/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-button
+    style="float: right; padding: 3px 0"
+    type="text"
+    @click="genLocalFile(srcFormCode,$event)"
+  >
+    生成代码
+  </el-button>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(["srcFormCode",'srcTableCode']),
+    srcCode(){
+      console.log("------rcCode -> this.$router", this.$route)
+      if(this.$route.path.includes('form')){
+        return this.srcFormCode
+      }
+
+      if(this.$route.path.includes('table')){
+        return this.srcTableCode
+      }
+      return ''
+    }
+  },
+  methods:{
+    genLocalFile(){
+console.log("TCL: genLocalFile -> this.srcCode", this.srcCode)
+    }
+  }
+}
+</script>
