7b19fc485e366b0cc14c2baea76a6286876d27fa
liugq <liugq5713@gmail.com>
2019-08-22 11:37:42 +0800

    fix[bin]

--- bin/toy.js
+++ bin/toy.js
@@ -2,37 +2,16 @@
 const exec = require("child_process").execSync;
 var program = require("commander");
 
-program
-  .version("0.1.0")
-  .option("-C, --chdir <path>", "change the working directory")
-  .option("-c, --config <path>", "set config path. defaults to ./deploy.conf")
-  .option("-T, --no-tests", "ignore test hook");
+program.version("0.1.0");
 
 program
-  .command("setup [env]")
+  .command("ui [env]")
   .description("run setup commands for all envs")
-  .option("-s, --setup_mode [mode]", "Which setup mode to use")
   .action(function(env, options) {
     var mode = options.setup_mode || "normal";
     env = env || "all";
     exec("npm run dev", { stdio: "inherit" });
     console.log("setup for %s env(s) with %s mode", env, mode);
-  });
-
-program
-  .command("exec <cmd>")
-  .alias("ex")
-  .description("execute the given remote cmd")
-  .option("-e, --exec_mode <mode>", "Which exec mode to use")
-  .action(function(cmd, options) {
-    console.log('exec "%s" using %s mode', cmd, options.exec_mode);
-  })
-  .on("--help", function() {
-    console.log("");
-    console.log("Examples:");
-    console.log("");
-    console.log("  $ deploy exec sequential");
-    console.log("  $ deploy exec async");
   });
 
 program.command("*").action(function(env) {
